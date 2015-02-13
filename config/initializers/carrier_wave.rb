5a56a1f4e9448795a15e9576db861f65a5cf295f
Tripp Davis <trippdavis3@gmail.com>
2015-02-13 02:12:42 +0000

    Add user following

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :aws_access_key_id     => ENV['AKIAIVJ4XPAP5KU3MZGA'],
+      :aws_secret_access_key => ENV['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']
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_directory     =  ENV['getbuckets']
   end
 end
